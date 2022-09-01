40a8ceb9b7fd6bc3738a82b278fd9712c4ee41a1
Jakob Borg <jakob@kastelo.net>
2022-09-01 08:22:40 +0200

    Add ViVa support

--- diveclime.go
+++ diveclime.go
@@ -1,66 +1,18 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-	"regexp"
-	"sort"
-	"strconv"
-	"strings"
-	"text/tabwriter"
+import "github.com/alecthomas/kong"
 
-	"github.com/gocolly/colly"
-)
+var cli struct {
+	HOV  bool   `short:"h" long:"havochvatten" description:"Use havochvatten.se instead of viva.sjofartsverket.se"`
+	ViVa string `short:"v" long:"viva" description:"Station(s) to fetch data for" default:"flinten"`
+}
 
 func main() {
-	flag.Parse()
+	kong.Parse(&cli)
 
-	var match *regexp.Regexp
-	var err error
-	if m := flag.Arg(0); m != "" {
-		match, err = regexp.Compile(m)
-		if err != nil {
-			fmt.Println("Matcher:", err)
-			os.Exit(1)
-		}
+	if cli.HOV {
+		havOchVatten()
+	} else {
+		viva(cli.ViVa)
 	}
-
-	c := colly.NewCollector()
-
-	temps := make(map[string]float64)
-	var key string
-
-	c.OnHTML("td[headers]", func(e *colly.HTMLElement) {
-		switch e.Attr("headers") {
-		case "head11":
-			key = strings.ReplaceAll(e.Text, "\u00ad", "")
-			if idx := strings.IndexAny(key, ","); idx > 0 {
-				key = key[:idx]
-			}
-		case "head13":
-			fields := strings.Fields(e.Text)
-			temps[key], _ = strconv.ParseFloat(fields[0], 64)
-		}
-	})
-	if err := c.Visit("https://www.havochvatten.se/badplatser-och-badvatten/vattenprov-badtemperatur/vattentemperatur-och-kvalitet-pa-badvatten-pa-sydkusten.html"); err != nil {
-		fmt.Println("Visit:", err)
-		os.Exit(1)
-	}
-
-	keys := make([]string, 0, len(temps))
-	for key := range temps {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
-
-	tw := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
-	for _, key := range keys {
-		arrow := ""
-		if match != nil && match.MatchString(key) {
-			arrow = "<--"
-		}
-		fmt.Fprintf(tw, "%s\t%4.1f\t%s\n", key, temps[key], arrow)
-	}
-	tw.Flush()
 }
---
+++ viva.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type vivaStation struct {
+	ID   int
+	Lat  float64
+	Lon  float64
+	Name string
+}
+
+type vivaStationsResponse struct {
+	Result struct {
+		Stations []vivaStation
+	} `json:"GetStationsResult"`
+}
+
+type vivaSample struct {
+	Calmh               int
+	Heading             int
+	Msg                 string
+	Name                string
+	Quality             string
+	StationID           int
+	Trend               string
+	Type                string
+	Unit                string
+	Updated             string
+	Value               string
+	WaterLevelOffset    float64
+	WaterLevelReference string
+}
+
+type vivaSamplesResponse struct {
+	Result struct {
+		Samples []vivaSample
+	} `json:"GetSingleStationResult"`
+}
+
+func viva(station string) error {
+	const stationsURL = "https://services.viva.sjofartsverket.se:8080/output/vivaoutputservice.svc/vivastation/"
+	res, err := http.Get(stationsURL)
+	if err != nil {
+		return err
+	}
+
+	var stations vivaStationsResponse
+	if err := json.NewDecoder(res.Body).Decode(&stations); err != nil {
+		return err
+	}
+
+	for _, s := range stations.Result.Stations {
+		if strings.Contains(strings.ToLower(s.Name), station) {
+			res, err := http.Get(stationsURL + strconv.Itoa(s.ID))
+			if err != nil {
+				return err
+			}
+
+			var samples vivaSamplesResponse
+			if err := json.NewDecoder(res.Body).Decode(&samples); err != nil {
+				return err
+			}
+
+			fmt.Println(s.Name)
+			fmt.Println(strings.Repeat("=", len(s.Name)))
+			for _, s := range samples.Result.Samples {
+				fmt.Printf("%s: %s %s\n", s.Name, s.Value, s.Unit)
+			}
+			fmt.Println()
+		}
+	}
+
+	return nil
+}
